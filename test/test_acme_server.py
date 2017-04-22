a63402c551105b8f6f497009d6fd5012bc08337b
Raúl Santos <borfast@gmail.com>
2017-04-22 23:08:43 +0100

    Remove forgotten demo code

--- test/test_acme_server.py
+++ test/test_acme_server.py
@@ -11,14 +11,3 @@ class TestAcmeServer(AcmeMegastoreTestCase):
         request = await self.client.request('GET', '/api/recent_purchases/')
         request.close()
         assert request.status == 404
-
-    # # a vanilla example
-    # def test_example(self):
-    #     async def test_get_route():
-    #         url = root + "/"
-    #         resp = await self.client.request("GET", url, loop=loop)
-    #         assert resp.status == 200
-    #         text = await resp.text()
-    #         assert "Hello, world" in text
-    #
-    #     self.loop.run_until_complete(test_get_route())
